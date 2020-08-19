ab6fbdc0ccff3fe8901d5507a9e624425fbb58a3
Paul Aguilar Zegada <a2019113015@estudiantes.upsa.edu.bo>
2020-08-19 19:58:03 -0400

    Ahora incluye la clase conversores, con pesos extra

---
+++ Número-a-letra/Número-a-letra/Controllers/LiteralController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Número_a_letra.Helpers;
+
+namespace Número_a_letra.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LiteralController : ControllerBase
+    {
+        [HttpGet]
+        public string literal_get(int numero)
+        {
+            return (Conversores.NumeroALetras(numero));
+        }
+        [HttpPost]
+        public string literal_get2([FromHeader] int numero)
+        {
+            return (Conversores.NumeroALetras(numero));
+        }
+    }
+}
